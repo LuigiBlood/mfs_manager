539d7d48f4f43404b3d2f49d525ad73bcc0661c7
LuigiBlood <luigiblood@gmail.com>
2019-08-26 15:01:25 +0200

    [GUI] Change Assembly Info

--- mfs_gui/Properties/AssemblyInfo.cs
+++ mfs_gui/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("mfs_gui")]
+[assembly: AssemblyTitle("64DD MFS Manager (GUI)")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("mfs_gui")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("64DD MFS Manager (GUI)")]
+[assembly: AssemblyCopyright("Copyright © LuigiBlood 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
