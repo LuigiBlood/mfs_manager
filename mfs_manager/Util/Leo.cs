9c97a788e4a21f4e91b71e07df4025326d737d0d
LuigiBlood <luigiblood@gmail.com>
2022-10-11 14:39:31 +0200

    Add MAME Format Utility Functions

--- mfs_manager/Util/Leo.cs
+++ mfs_manager/Util/Leo.cs
@@ -13,13 +13,29 @@ namespace mfs_manager
         public const int SYSTEM_LBAS = 24;
         public const int DISKID_LBA = 14;
 
-        const int SECTORS_PER_BLOCK = 85;
+        public const int SECTORS_PER_BLOCK = 85;
+
+        public const int BLOCKS_PER_TRACK = 2;
+
+        /* LBA to Disk System Data (Retail) */
+        public static readonly int[] LBA_SYS_PROD = { 0, 1, 8, 9 };
+
+        /* LBA to Disk System Data (Development) */
+        public static readonly int[] LBA_SYS_DEV = { 2, 3, 10, 11 };
 
         /* Sector Size in bytes [zone] */
-        static readonly int[] SECTOR_SIZE = { 232, 216, 208, 192, 176, 160, 144, 128, 112 };
+        public static readonly int[] SECTOR_SIZE = { 232, 216, 208, 192, 176, 160, 144, 128, 112 };
 
         /* Block Size in bytes [zone] */
         public static readonly int[] BLOCK_SIZE = { 0x4D08, 0x47B8, 0x4510, 0x3FC0, 0x3A70, 0x3520, 0x2FD0, 0x2A80, 0x2530 };
+
+        /* Outer Track per Zone [zone] */
+        static readonly int[] PZONE_TRACK = { 0x000, 0x09E, 0x13C, 0x1D1, 0x266, 0x2FB, 0x390, 0x425, 0x497 };
+
+        /* Start Track of Zone [pzone] */
+        static readonly int[] TRACK_START_ZONE_TBL =
+            {0x000, 0x09E, 0x13C, 0x1D1, 0x266, 0x2FB, 0x390, 0x425,
+             0x091, 0x12F, 0x1C4, 0x259, 0x2EE, 0x383, 0x418, 0x48A};
 
         /* LBA to VZone [type,vzone] */
         static readonly int[,] VZONE_LBA_TBL = {
@@ -99,6 +115,76 @@ namespace mfs_manager
                 }
             }
             return totalbytes;
+        }
+
+        /* MAME Disk Format Offsets for each zone */
+        static readonly int[] MAMEOffsetTable =
+            {0x0,0x5F15E0,0xB79D00,0x10801A0,0x1523720,0x1963D80,0x1D414C0,0x20BBCE0,
+             0x23196E0,0x28A1E00,0x2DF5DC0,0x3299340,0x36D99A0,0x3AB70E0,0x3E31900,0x4149200};
+
+        public static int LBAToMAMEOffset(int lba, byte[] sysData)
+        {
+            int head, track, block;
+            LBAToPhys(lba, sysData, out head, out track, out block);
+            return PhysToMAMEOffset(head, track, block, 0);
+        }
+        
+        public static int PhysToMAMEOffset(int head, int track, int block, int sector)
+        {
+            int pzone = Array.FindIndex(PZONE_TRACK, x => track < x) - 1;
+
+            int trackRelative = track - PZONE_TRACK[pzone];
+
+            int offsetCalc = MAMEOffsetTable[pzone + (head * 8)];
+            offsetCalc += BLOCK_SIZE[pzone] * 2 * trackRelative;
+            offsetCalc += block * BLOCK_SIZE[pzone];
+            offsetCalc += sector * SECTOR_SIZE[pzone];
+
+            return offsetCalc;
+        }
+
+        public static void LBAToPhys(int lba, byte[] sysData, out int head, out int track, out int block)
+        {
+            //Get Disk Type
+            int diskType = sysData[0x05] & 0x0F;
+
+            //Get Block
+            if (((lba & 3) == 0) || ((lba & 3) == 3))
+                block = 0;
+            else
+                block = 1;
+
+            //Calculate Head & Track
+            int VZone = LBAToVZone(lba, diskType);
+            int PZone = VZoneToPZone(VZone, diskType);
+
+            //Get Head
+            head = (7 < PZone) ? 1 : 0;
+
+            int calcPZone = PZone - (head * 7);
+
+            int calcTrack = (lba - ((VZone == 0) ? 0 : VZONE_LBA_TBL[diskType, VZone - 1])) / BLOCKS_PER_TRACK;
+
+            //int zoneTrackStart = PZONE_TRACK[calcPZone - head];
+            int zoneTrackStart = TRACK_START_ZONE_TBL[PZone];
+            if (head == 1)
+            {
+                calcTrack = -calcTrack;
+                zoneTrackStart = PZONE_TRACK[calcPZone - head];
+            }
+            calcTrack += TRACK_START_ZONE_TBL[PZone];
+
+            int calcDefectOffset = (PZone == 0) ? 0 : sysData[8 + PZone - 1];
+            int calcDefectAmount = sysData[8 + PZone] - calcDefectOffset;
+
+            while ((calcDefectAmount > 0) && ((sysData[0x20 + calcDefectOffset] + zoneTrackStart) <= calcTrack))
+            {
+                calcTrack++;
+                calcDefectOffset++;
+                calcDefectAmount--;
+            }
+
+            track = calcTrack;
         }
     }
 }
