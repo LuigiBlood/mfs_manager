70d4c5c70ff4c5173d0a60cc943b493af966829c
LuigiBlood <luigiblood@gmail.com>
2022-11-01 18:45:29 +0100

    Add check for MFS Filesystem in mfs_manager cmd line

--- mfs_manager/Program.cs
+++ mfs_manager/Program.cs
@@ -34,7 +34,13 @@ namespace mfs_manager
 
                 if (mfsDisk.Disk.Format == LeoDisk.DiskFormat.Invalid)
                 {
-                    Console.WriteLine("Error loading RAM file");
+                    Console.WriteLine("Disk file is invalid.");
+                    return;
+                }
+
+                if (mfsDisk.Disk.RAMFileSystem != LeoDisk.FileSystem.MFS)
+                {
+                    Console.WriteLine("Disk file is not using MFS.");
                     return;
                 }
 
